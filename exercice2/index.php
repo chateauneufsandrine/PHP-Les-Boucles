8aad75b4c825b958f6882709fe65b7dd0b5dc533
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-24 11:34:20 +0100

    add exercice2

--- exercice2/index.php
+++ exercice2/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // EXERCICE2///////////////////////////////////////////////////////////////
 // Créer deux variables. Initialiser la première à 0 et la deuxième avec un nombre compris en 1 et 100.
 // Tant que la première variable n'est pas supérieure à 20 :
@@ -6,18 +6,36 @@
 // multiplier la première variable avec la deuxième
 // afficher le résultat
 // incrementer la première variable
+$var1 = 0;
+$var2 = 40;
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <?php function multiplier($var1, $var2)
+    { ?>
+        <?php return $var1 * $var2; ?>
+
+    <?php  } ?>
+
+        <?php while ($var1 < 20) { ?>
+            <?php $resultat = $var1 * $var2 ?>
+           <h2><?= $resultat ?></h2> 
+            <?php $var1 += 1 ?>
+        <?php } ?>
+
+
+
 </body>
+
 </html>
