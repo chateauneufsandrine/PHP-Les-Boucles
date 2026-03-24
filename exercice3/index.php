a5af38a41ced41f2f079ba6817b3ad6f57f7a66d
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-24 11:50:01 +0100

    add exercice3

--- exercice3/index.php
+++ exercice3/index.php
@@ -6,10 +6,10 @@
 // multiplier la première variable avec la deuxième
 // afficher le résultat
 // décrémenter la première variable
-
+$var1 = 100;
+$var2 = 2;
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,6 +19,14 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php function muliplier($var1, $var2) {?>
+    <?php return $var1*$var2 ?>
+   <?php } ?> 
+    <?php while ($var1 >=10 ) {
+    ?>
+    <h2><?php $resultat = $var1*$var2 ?></h2>
+    <?= $resultat ?>
+    <?php $var1 -=1 ?>
+    <?php  } ?>
 </body>
 </html>
