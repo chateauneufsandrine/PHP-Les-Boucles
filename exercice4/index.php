a9908efd574df9c4b3514153f5216a43f5b0ee0f
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-24 12:01:12 +0100

    add exercice4

--- exercice4/index.php
+++ exercice4/index.php
@@ -1,21 +1,30 @@
-<?php 
+<?php
 // EXERCICE4/////////////////////////////////////////////////////////////////////
 // Créer une variable et l'initialiser à 1.
 // Tant que cette variable n'atteint pas 10, il faut :
 
 // l'afficher
 // l'incrementer de la moitié de sa valeur
+$var1 = 1;
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
+    <?php
+    while ($var1 < 10) {
+    ?>
+        <h2><?= $var1 += $var1 / 2 ?> </h2>
+    <?php }  ?>
 </body>
+
 </html>
