79287cf84cea1a41f885b2790fc33c823b91b95a
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-24 12:11:45 +0100

    add exercice6

--- exercice6/index.php
+++ exercice6/index.php
@@ -1,16 +1,22 @@
-<?php 
+<?php
 // EXERCICE6////////////////////////////////////////////////////////////////////////////
 // En allant de 20 à 0 avec un pas de 1, afficher le message C'est presque bon
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <?php for ($i = 20; $i >= 0; $i--) {
+    ?>
+        <h3>C'est presque bon</h3>
+    <?php }  ?>
 </body>
+
 </html>
