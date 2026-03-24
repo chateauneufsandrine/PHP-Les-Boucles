e36d7b19cee53dee045784b75014b83559aad5fd
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-24 12:07:54 +0100

    add exercice5

--- exercice5/index.php
+++ exercice5/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // EXERCICE5////////////////////////////////////////////////////////////////////////////
 // En allant de 1 à 15 avec un pas de 1, afficher le message On y arrive presque.
 
@@ -6,12 +6,20 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <?php for ($i = 0; $i < 15; $i++) {
+    ?>
+        <h3>On y arrive presque</h3>
+
+    <?php  }  ?>
+
 </body>
+
 </html>
